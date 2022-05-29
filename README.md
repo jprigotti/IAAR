1713ffbcd38e4a84f17b69ea0a30a2660a8147ba
Juan Pablo Rigotti <rigottijp@gmail.com>
2022-05-29 18:21:11 -0300

    first working version

---
+++ README.md
@@ -0,0 +1,29 @@
+# CaC_TP_Integrador_1_final
+Desde este link se accede a la página publicada en Github
+https://jprigotti.github.io/CaC_TP_Integrador_1_final/
+
+<hr>
+
+Haciendo click en el ícono CaC se accede a una página que contiene cada sección dentro de un iframe y un Acordion de Bootstrap muestra el código HTML/CSS, también se puede ver aquí https://jprigotti.github.io/CaC_TP_Integrador_1_final/code.html
+
+<hr>
+
+Se grabaron 9 videos, el primero es una Intro, y los sucesivos muestran como hacer cada sección de la página, y los últimos 2 cubren Github.
+<ul>
+  <li>Video 1: Intro https://youtu.be/DbtR0VDS35c</li>
+  <li>Video 2: Navbar con Bootstrap https://youtu.be/uFyPPUtJ8qo</li>
+  <li>Video 3: Showcase https://youtu.be/XvGpPmuSHi0</li>
+  <li>Video 4: Oradores/Cards https://youtu.be/hGJR1OE8SPA</li>
+  <li>Video 5: Call to action https://youtu.be/E1PS5SKYDYE</li>
+  <li>Video 6: Form https://youtu.be/9MQmUa6iddM</li>
+  <li>Video 7: Footer https://youtu.be/CXTBNFOGsHc</li>
+  <li>Video 8: Github Part1 https://youtu.be/zGpQKUKFW4Y</li>
+  <li>Video 9: Github Part2 https://youtu.be/uEXoUa6NQ60</li>
+
+
+<hr>
+Mayo 25, 2022 se agregó la sección Tickets a index.html y code.html
+
+<li>Video 10: Tickets section con JavaScript https://www.youtube.com/watch?v=AD71yUacREA&list=PLryrs3RkRBTBamEuAc1k1laYmaz8-G50P&index=10</li>
+</ul>
+
---
+++ parallax.js
@@ -0,0 +1,145 @@
+// for heading
+window.onload = function () {
+  // let navNode = document.querySelector(".navbar-brand");
+  // let spanNode = document.createElement("span");
+  // spanNode.innerHTML = `<span class="navbarImg"><img src="./images/IAAR.png" alt="Logo CaC"></span>`;
+  // spanNode.className = 'navbarImg';
+  // navNode.append(spanNode);
+}
+
+window.addEventListener('resize', function (event) {
+  let newWidth = window.innerWidth;
+
+  if (newWidth < 992){
+    $(".navbarImg").css({
+      display: "none",
+    });
+  }else{
+    console.log("mostrar IMG");
+    $(".navbarImg").css({
+      display: "contents",
+    });
+    
+  }
+});
+  
+
+$(window).scroll(function () {
+  //Código para el parallax del Showcase
+  let imgHeight = document.querySelector(".showcase");
+
+  const
+    a = $(this).scrollTop(),
+    b = imgHeight.offsetHeight;
+  // console.log(`a= ${a}, b= ${b}, a/b)${a / b}`);
+
+  $(".parallax").css({
+    top: a / 1.6 + "px",
+    //opacity:  1 - a / b
+  });
+
+  // Este código agrega o remueve una clase dependiendo del scroll en pantalla
+  // let navNode = document.querySelector(".navbar-brand");
+  // let spanNode = document.createElement("span");
+  // spanNode.innerHTML = `<span class="navbarImg order-1"><a href="./code.html" target="_blank"><img src="./images/IAAR.png" alt="Logo CaC"></a></span>`;
+  // spanNode.className = 'navbarImg';
+  // if (a < 600) {
+  //   if (!document.querySelector(".navbarImg")) navNode.append(spanNode);
+
+  // } else {
+  //   if (document.querySelector(".navbarImg")) document.querySelector(".navbarImg").remove();
+  // }
+
+  //
+  $(".navbarImg img").css({
+    width: `${100 * (1 - (a / b))}`,
+    transform: `translateX(-${a / 3}px)`
+  });
+
+
+  $(".parallax").css({
+    top: a / 1.6 + "px",
+    //opacity:  1 - a / b
+  });
+
+  $(".showcase .shadow").css({
+    // backgroundColor: `rgba(73,80,87, ${2*(a/b)})`
+  });
+
+  // $(".content").css({
+  //   bottom: a * 0.5 + "px",
+  //   //opacity: 1 - a / b 
+  // });
+
+
+
+  $(".navbar").css({
+    backgroundColor: `rgba(52,58,64, ${2 * a / b})`
+  });
+});
+
+// Parallax 2 ArtGallery
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+  var screenHeight = $(window).height();
+
+  console.log(`scrollTop: ${scroll}, screenHeight: ${screenHeight}`);
+
+  $('.parallax2').each(function () {
+    var offset = $(this).offset().top;
+    var distanceFromBottom = offset - scroll - screenHeight
+
+    if (offset > screenHeight && offset) {
+      $(this).css('background-position', 'center ' + ((distanceFromBottom) * 0.3) + 'px');
+    } else {
+      $(this).css('background-position', 'center ' + ((-scroll) * 0.3) + 'px');
+    }
+  })
+});
+
+document.addEventListener("scroll", () => {
+  const
+    top = window.pageYOffset,
+    one = document.querySelector(".one"),
+    two = document.querySelector(".two"),
+    three = document.querySelector(".three"),
+    four = document.querySelector(".four"),
+    five = document.querySelector(".five");
+
+
+});
+
+// Código para expandir los Submenue
+let navLink = document.querySelector(".navLink1");
+navLink.addEventListener("mouseover", expandDrowpdownMenu);
+navLink.addEventListener("mouseleave", compressDrowpdownMenu);
+
+// // let navBarSubMenu = document.querySelector(".navBarSubMenu .nav-link");
+// let navBarSubMenu = document.querySelector(".navBarSubMenu");
+// navBarSubMenu.addEventListener("mouseover", expandSubMenu);
+// navBarSubMenu.addEventListener("mouseleave", compressSubMenu);
+
+function expandDrowpdownMenu() {
+  $(".dropDown1").css({
+    display: "contents",
+  });
+}
+
+function compressDrowpdownMenu() {
+  $(".dropDown1").css({
+    display: "none"
+  });
+}
+
+// function expandSubMenu(){
+//   $(".navBarSubMenuSH").css({
+//     display: "contents"
+//   });
+// }
+
+
+// function compressSubMenu(){
+//   $(".navBarSubMenuSH").css({
+//     display: "none"
+//   });
+// }
