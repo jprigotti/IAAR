a7111c6005edf63662d3a0b4aabe46fe1ddc9864
Juan Pablo Rigotti <rigottijp@gmail.com>
2022-05-31 23:18:04 -0300

    adjusting carousel and responsive

--- indexJS.js
+++ indexJS.js
@@ -6,71 +6,76 @@ window.addEventListener('resize', windowWidthChange);
 
 var allCards = document.querySelectorAll('.card');
 for (var i = 0; i < allCards.length; i++) {
-    var self = allCards[i];
+  var self = allCards[i];
 
-    self.addEventListener('mouseover', function (evento) {  
-        // prevent browser's default action
-        evento.preventDefault();
+  self.addEventListener('mouseover', function (evento) {
+    // prevent browser's default action
+    evento.preventDefault();
 
-        // call your awesome function here
-        cardDisplay(this.className.slice(5, 10)); // 'this' refers to the current button on for loop
-    }, false);
+    // call your awesome function here
+    cardDisplay(this.className.slice(5, 11)); // 'this' refers to the current button on for loop
+  }, false);
 
 }
 
 var allCards = document.querySelectorAll('.card');
 for (var i = 0; i < allCards.length; i++) {
-    var self = allCards[i];
+  var self = allCards[i];
 
-    self.addEventListener('mouseleave', function (evento) {  
-      // prevent browser's default action
-      evento.preventDefault();
+  self.addEventListener('mouseleave', function (evento) {
+    // prevent browser's default action
+    evento.preventDefault();
 
-      // call your awesome function here
-      cardHide(this.className.slice(5, 10)); // 'this' refers to the current button on for loop
+    // call your awesome function here
+    cardHide(this.className.slice(5, 11)); // 'this' refers to the current button on for loop
   }, false);
 }
 
-function cardDisplay(card){
+function cardDisplay(card) {
   $(`.${card} .author`).css({
     display: "contents",
   });
-//   cardTitle=document.querySelector(`.${card} .author`);
-// cardTitle.classList.remove('hidden');
-// setTimeout(function () {
-//   cardTitle.classList.remove('visuallyhidden');
-// }, 20);
+  //   cardTitle=document.querySelector(`.${card} .author`);
+  // cardTitle.classList.remove('hidden');
+  // setTimeout(function () {
+  //   cardTitle.classList.remove('visuallyhidden');
+  // }, 20);
 }
 
-function cardHide(card){
+function cardHide(card) {
   $(`.${card} .author`).css({
     display: "none",
   });
   // cardTitle=document.querySelector(`.${card} .author`);
-// cardTitle.classList.add('visuallyhidden');    
-// cardTitle.addEventListener('transitionend', function(e) {
-//   cardTitle.classList.add('hidden');
-// }, {
-//   capture: false,
-//   once: true,
-//   passive: false
-// });
+  // cardTitle.classList.add('visuallyhidden');    
+  // cardTitle.addEventListener('transitionend', function(e) {
+  //   cardTitle.classList.add('hidden');
+  // }, {
+  //   capture: false,
+  //   once: true,
+  //   passive: false
+  // });
 
 }
 
 
 // Codigo para el scroll del Carousel
 const fila = document.querySelector(".containerCarousel");
+const card = document.querySelector(".card");
+
 
 const leftArrow = document.querySelector("#leftArrow");
 const rightArrow = document.querySelector("#rightArrow");
 
 rightArrow.addEventListener("click", () => {
-  fila.scrollLeft += fila.offsetWidth;
+  console.log(`fila ${fila.offsetWidth}, card ${card.offsetWidth}`)
+  // fila.scrollLeft =+ card.offsetWidth*(fila.offsetWidth/card.offsetWidth);
+  fila.scrollLeft += card.offsetWidth;
 })
 
 leftArrow.addEventListener("click", () => {
-  fila.scrollLeft -= fila.offsetWidth;
+  // fila.scrollLeft =- card.offsetWidth*(fila.offsetWidth/card.offsetWidth);
+  fila.scrollLeft -= card.offsetWidth;
 })
 
 
@@ -79,9 +84,9 @@ function windowWidthChange(evento) {
   const
     a = $(this).scrollTop(),
     b = imgHeight.offsetHeight;
-    console.log(`a is ${a}`)
+  console.log(`a is ${a}`)
 
-    
+
   // MEDIA QUERY < 576//
   if (window.innerWidth < 576) {
 
@@ -105,11 +110,11 @@ function windowWidthChange(evento) {
     $(".publicaciones .card").css({
       minWidth: "100%"
     });
-  
 
+    fila.scrollLeft = 0;
 
   } else if (window.innerWidth < 992) {
-  // MEDIA QUERY < 992//
+    // MEDIA QUERY < 992//
 
     //Remove Logo Image
     $(".navbarImg").css({
@@ -120,27 +125,29 @@ function windowWidthChange(evento) {
     $(".navbar").css({
       backgroundColor: `rgba(52,58,64, 1)`
     });
-    
+
     //Código para el parallax del Showcase
     $(".parallax").css({
       top: a / 1.6 + "px",
     });
 
     $(".publicaciones .card").css({
-      minWidth: "50%"
+      minWidth: "100%"
     });
 
-  } else {
-  // MEDIA QUERY > 992//
+    fila.scrollLeft = 0;
 
-     //Add Logo Image
+  } else if (window.innerWidth < 1420) {
+    // MEDIA QUERY < 1420//
+
+    //Add Logo Image
     $(".navbarImg").css({
       display: "contents",
     });
 
-      $(".parallax").css({
-        top: a / 1.6 + "px",
-      });
+    $(".parallax").css({
+      top: a / 1.6 + "px",
+    });
 
 
       $(".navbar img").css({
@@ -148,15 +155,39 @@ function windowWidthChange(evento) {
         transform: `translateX(-${a / 3}px)`
       });
 
+    $(".navbar").css({
+      backgroundColor: `rgba(52,58,64, ${2 * a / b})`
+    });
 
-      $(".navbar").css({
-        backgroundColor: `rgba(52,58,64, ${2 * a / b})`
+    $(".publicaciones .card").css({
+      minWidth: "33.33%"
+    });
+
+  } else{
+    // MEDIA QUERY > 992//
+
+    $(".navbarImg").css({
+      display: "contents",
+    });
+
+    $(".parallax").css({
+      top: a / 1.6 + "px",
+    });
+
+
+      $(".navbar img").css({
+        width: `${100 * (1 - (a / b))}`,
+        transform: `translateX(-${a / 3}px)`
       });
 
-      $(".publicaciones .card").css({
-        minWidth: "33.33%"
-      });
-
+    $(".navbar").css({
+      backgroundColor: `rgba(52,58,64, ${2 * a / b})`
+    });
+    
+    $(".publicaciones .card").css({
+      minWidth: "25%"
+    });
+    fila.scrollLeft = 0;
   }
 }
 
@@ -273,7 +304,7 @@ function compressDrowpdownMenu() {
 
 
 
-  
 
 
-  
+
+
